69087e183b0d15b3a89dac038497ed671b745ac9
Nick McMahon <nicholas.mcmahon@digital.justice.gov.uk>
2021-03-16 14:04:49 +0000

    set up queues for automated testing.

--- src/test/resources/localstack/setup-sns.sh
+++ src/test/resources/localstack/setup-sns.sh
@@ -4,13 +4,13 @@ export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=eu-west-2
 aws --endpoint-url=http://localhost:4575 sns create-topic --name hmpps-domain-events
 
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name hmpps_registers_to_delius_dlq
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name hmpps_registers_to_delius_queue
-aws --endpoint-url=http://localhost:4576 sqs set-queue-attributes --queue-url "http://localhost:4576/queue/hmpps_registers_to_delius_queue" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"3\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:hmpps_registers_to_delius_dlq\"}"}'
+aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name hmpps_tier_to_delius_dlq
+aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name hmpps_tier_to_delius_queue
+aws --endpoint-url=http://localhost:4576 sqs set-queue-attributes --queue-url "http://localhost:4576/queue/hmpps_tier_to_delius_queue" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"3\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:hmpps_tier_to_delius_dlq\"}"}'
 aws --endpoint-url=http://localhost:4575 sns subscribe \
     --topic-arn arn:aws:sns:eu-west-2:000000000000:hmpps-domain-events \
     --protocol sqs \
-    --notification-endpoint http://localhost:4576/queue/hmpps_registers_to_delius_queue \
-    --attributes '{"FilterPolicy":"{\"eventType\":[ \"COURT_REGISTER_UPDATE\", \"COURT_REGISTER_INSERT\"] }"}'
+    --notification-endpoint http://localhost:4576/queue/hmpps_tier_to_delius_queue \
+    --attributes '{"FilterPolicy":"{\"eventType\":[ \"HMPPS_TIER_CALCULATION_COMPLETE\""] }"}'
 
 echo All Ready
