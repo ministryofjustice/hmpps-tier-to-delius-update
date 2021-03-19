f0a8a3c309676fa97dfbab7eaffcb0d8e7ccf39d
Nick McMahon <nicholas.mcmahon@digital.justice.gov.uk>
2021-03-19 11:39:21 +0000

    Try sqs listener instead of JMS

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstiertodeliusupdate/config/AwsConfiguration.kt
@@ -0,0 +1,47 @@
+package uk.gov.justice.digital.hmpps.hmppstiertodeliusupdate.config
+
+import com.amazonaws.auth.AWSCredentials
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.sns.AmazonSNSAsync
+import com.amazonaws.services.sns.AmazonSNSAsyncClientBuilder
+import com.amazonaws.services.sqs.AmazonSQSAsync
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory
+import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+
+@EnableSqs
+@Configuration
+@ConditionalOnProperty(name = ["sqs.provider"], havingValue = "aws")
+class AwsConfiguration(
+  @Value("\${sqs.aws.access-key-id}") val accessKeyId: String,
+  @Value("\${sqs.aws.secret-access-key}") val secretKey: String,
+  @Value("\${sqs.endpoint.region}") val region: String,
+) {
+
+  @Primary
+  @Bean
+  fun amazonSQSAsync(): AmazonSQSAsync {
+    val credentials: AWSCredentials = BasicAWSCredentials(accessKeyId, secretKey)
+    return AmazonSQSAsyncClientBuilder
+      .standard()
+      .withRegion(region)
+      .withCredentials(AWSStaticCredentialsProvider(credentials)).build()
+  }
+
+  @Primary
+  @Bean
+  fun simpleMessageListenerContainerFactory(amazonSQSAsync: AmazonSQSAsync):
+    SimpleMessageListenerContainerFactory {
+      val factory = SimpleMessageListenerContainerFactory()
+      factory.setAmazonSqs(amazonSQSAsync)
+      factory.setMaxNumberOfMessages(1)
+      factory.setWaitTimeOut(20)
+      return factory
+    }
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppstiertodeliusupdate/config/AwsLocalStackConfiguration.kt
@@ -0,0 +1,44 @@
+package uk.gov.justice.digital.hmpps.hmppstiertodeliusupdate.config
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.AnonymousAWSCredentials
+import com.amazonaws.client.builder.AwsClientBuilder
+import com.amazonaws.services.sqs.AmazonSQSAsync
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory
+import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+
+@EnableSqs
+@Configuration
+@ConditionalOnProperty(name = ["sqs.provider"], havingValue = "localstack")
+class AwsLocalStackConfiguration(
+  @Value("\${sqs.endpoint.region}") val region: String
+) {
+
+  @Primary
+  @Bean
+  fun amazonSQSAsync(
+    @Value("\${sqs.endpoint.url}") serviceEndpoint: String
+  ): AmazonSQSAsync {
+    return AmazonSQSAsyncClientBuilder.standard()
+      .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(serviceEndpoint, region))
+      .withCredentials(AWSStaticCredentialsProvider(AnonymousAWSCredentials()))
+      .build()
+  }
+
+  @Primary
+  @Bean
+  fun simpleMessageListenerContainerFactory(amazonSQSAsync: AmazonSQSAsync):
+    SimpleMessageListenerContainerFactory {
+      val factory = SimpleMessageListenerContainerFactory()
+      factory.setAmazonSqs(amazonSQSAsync)
+      factory.setMaxNumberOfMessages(1)
+      factory.setWaitTimeOut(20)
+      return factory
+    }
+}
