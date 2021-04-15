5e4518873e6f6c0d8a974255fac59da9c2f354dc
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-04-15 17:52:05 +0100

    MAS-398 ensure scope is not set in oauth client config

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstiertodeliusupdate/integration/endtoend/MockedEndpointsTestBase.kt
@@ -0,0 +1,70 @@
+package uk.gov.justice.digital.hmpps.hmppstiertodeliusupdate.integration.endtoend
+
+import com.amazonaws.services.sqs.AmazonSQS
+import com.amazonaws.services.sqs.model.PurgeQueueRequest
+import com.google.gson.Gson
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.matches
+import org.awaitility.kotlin.untilCallTo
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.mockserver.integration.ClientAndServer
+import org.mockserver.model.HttpRequest
+import org.mockserver.model.HttpResponse
+import org.mockserver.model.MediaType
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest()
+@ActiveProfiles("test")
+abstract class MockedEndpointsTestBase {
+  @Qualifier("awsSqsClient")
+  @Autowired
+  internal lateinit var awsSqsClient: AmazonSQS
+
+  @Value("\${sqs.queue}")
+  lateinit var queue: String
+
+  var hmppsTier: ClientAndServer = ClientAndServer.startClientAndServer(8091)
+  var communityApi: ClientAndServer = ClientAndServer.startClientAndServer(8092)
+
+  private var oauthMock: ClientAndServer = ClientAndServer.startClientAndServer(9090)
+
+  private val gson: Gson = Gson()
+
+  @BeforeEach
+  fun before() {
+    awsSqsClient.purgeQueue(PurgeQueueRequest(queue))
+    await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
+    setupOauth()
+  }
+
+  @AfterEach
+  fun reset() {
+    hmppsTier.reset()
+    communityApi.reset()
+    oauthMock.reset()
+  }
+
+  @AfterAll
+  fun tearDownServer() {
+    hmppsTier.stop()
+    communityApi.stop()
+    oauthMock.stop()
+  }
+
+  fun setupOauth() {
+    val response = HttpResponse.response().withContentType(MediaType.APPLICATION_JSON)
+      .withBody(gson.toJson(mapOf("access_token" to "ABCDE", "token_type" to "bearer")))
+    oauthMock.`when`(HttpRequest.request().withPath("/auth/oauth/token").withBody("grant_type=client_credentials")).respond(response)
+  }
+
+  fun getNumberOfMessagesCurrentlyOnQueue(): Int? {
+    val queueAttributes = awsSqsClient.getQueueAttributes(queue, listOf("ApproximateNumberOfMessages"))
+    return queueAttributes.attributes["ApproximateNumberOfMessages"]?.toInt()
+  }
+}
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstiertodeliusupdate/integration/endtoend/TierTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstiertodeliusupdate/integration/endtoend/TierTest.kt
@@ -1,65 +1,32 @@
 package uk.gov.justice.digital.hmpps.hmppstiertodeliusupdate.integration.endtoend
 
-import com.amazonaws.services.sqs.AmazonSQS
 import org.awaitility.kotlin.await
 import org.awaitility.kotlin.matches
 import org.awaitility.kotlin.untilCallTo
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
-import org.mockserver.integration.ClientAndServer
-import org.mockserver.integration.ClientAndServer.startClientAndServer
 import org.mockserver.model.HttpRequest
 import org.mockserver.model.HttpRequest.request
 import org.mockserver.model.HttpResponse.response
 import org.mockserver.model.MediaType.APPLICATION_JSON
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 import uk.gov.justice.digital.hmpps.hmppstiertodeliusupdate.helpers.tierUpdateMessage
 
-@SpringBootTest
-@ActiveProfiles("test")
 @TestInstance(PER_CLASS)
-class TierTest {
-  @Qualifier("awsSqsClient")
-  @Autowired
-  internal lateinit var awsSqsClient: AmazonSQS
-
-  @Value("\${sqs.queue}")
-  lateinit var queue: String
-
-  private var hmppsTier: ClientAndServer = startClientAndServer(8091)
-  private var communityApi: ClientAndServer = startClientAndServer(8092)
-
-  @AfterEach
-  fun reset() {
-    hmppsTier.reset()
-    communityApi.reset()
-  }
-
-  @AfterAll
-  fun tearDownServer() {
-    hmppsTier.stop()
-    communityApi.stop()
-  }
+class TierTest : MockedEndpointsTestBase() {
 
   @Test
   fun `will consume a TIER_CALCULATION_COMPLETE message, retrieve calculation and send update to community-api`() {
     setupTierCalculationResponse()
+
     val message = tierUpdateMessage()
 
-    await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
     val tierWriteback = request().withPath("/offenders/crn/12345/tier/B3").withMethod("POST")
     setupTierWriteback(tierWriteback)
-
     awsSqsClient.sendMessage(queue, message)
 
     await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
+    Thread.sleep(1000L) // need to wait until the processing has finished. There must be a better way
     communityApi.verify(tierWriteback)
   }
 
@@ -73,10 +40,5 @@ class TierTest {
     communityApi.`when`(tierWriteback).respond(
       response().withStatusCode(200)
     )
-  }
-
-  fun getNumberOfMessagesCurrentlyOnQueue(): Int? {
-    val queueAttributes = awsSqsClient.getQueueAttributes(queue, listOf("ApproximateNumberOfMessages"))
-    return queueAttributes.attributes["ApproximateNumberOfMessages"]?.toInt()
   }
 }
