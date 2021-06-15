4e9fd4b40cc65cd36515411d32f831ea899725b6
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-06-15 12:01:23 +0100

    MAS-420 test coverage for errors other than 404

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstiertodeliusupdate/integration/endtoend/TierTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstiertodeliusupdate/integration/endtoend/TierTest.kt
@@ -14,7 +14,7 @@ import org.mockserver.model.HttpResponse.response
 import org.mockserver.model.MediaType.APPLICATION_JSON
 import org.mockserver.model.RequestDefinition
 import uk.gov.justice.digital.hmpps.hmppstiertodeliusupdate.helpers.tierUpdateMessage
-import java.nio.file.Files
+import java.nio.file.Files.readString
 import java.nio.file.Paths
 
 internal class TierTest : MockedEndpointsTestBase() {
@@ -62,6 +62,20 @@ internal class TierTest : MockedEndpointsTestBase() {
   }
 
   @Test
+  fun `leaves message on queue when server error response from community-api`() {
+    setupTierCalculationResponse()
+
+    val tierWriteback = request().withPath("/offenders/crn/12345/tier/B3").withMethod("POST")
+    setupTierWritebackServerError(tierWriteback)
+    awsSqsClient.sendMessage(queue, tierUpdateMessage())
+
+    await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
+    (await withPollInterval ONE_HUNDRED_MILLISECONDS).ignoreException(IllegalArgumentException::class)
+      .untilAsserted { communityApi.verify(tierWriteback) }
+    await untilCallTo { getNumberOfMessagesCurrentlyNotVisibleOnQueue() } matches { it == 1 }
+  }
+
+  @Test
   fun `leaves message on queue if tier calculation cannot be found`() {
     var notFoundRequest = setupNotFoundTierCalculationResponse()
     awsSqsClient.sendMessage(queue, tierUpdateMessage())
@@ -86,24 +100,26 @@ internal class TierTest : MockedEndpointsTestBase() {
     return request
   }
 
-  private fun setupTierWriteback(tierWriteback: HttpRequest) {
+  private fun setupTierWriteback(tierWriteback: HttpRequest) =
     communityApi.`when`(tierWriteback).respond(
       response().withStatusCode(200)
     )
-  }
 
-  private fun setupTierWritebackOffenderNotFound(tierWriteback: HttpRequest) {
+  private fun setupTierWritebackServerError(tierWriteback: HttpRequest) =
     communityApi.`when`(tierWriteback).respond(
-      response().withStatusCode(404).withBody(responseFrom("src/test/resources/fixtures/community-api/offender-404.json"))
+      response().withStatusCode(500)
     )
-  }
 
-  private fun setupTierWritebackReasonNotFound(tierWriteback: HttpRequest) {
+  private fun setupTierWritebackOffenderNotFound(tierWriteback: HttpRequest) =
+    communityApi.`when`(tierWriteback).respond(
+      response().withStatusCode(404)
+        .withBody(responseFrom("src/test/resources/fixtures/community-api/offender-404.json"))
+    )
+
+  private fun setupTierWritebackReasonNotFound(tierWriteback: HttpRequest) =
     communityApi.`when`(tierWriteback).respond(
       response().withStatusCode(404).withBody(responseFrom("src/test/resources/fixtures/community-api/reason-404.json"))
     )
-  }
 
-  private fun responseFrom(path: String) =
-    Files.readString(Paths.get(path))
+  private fun responseFrom(path: String) = readString(Paths.get(path))
 }
