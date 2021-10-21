13780c31eb0047152da086e1b7c50bc481046a08
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-10-21 08:21:53 +0100

    Add messageAttributes to the tier calculation complete message

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstiertodeliusupdate/helpers/Messages.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstiertodeliusupdate/helpers/Messages.kt
@@ -1,15 +1,22 @@
 package uk.gov.justice.digital.hmpps.hmppstiertodeliusupdate.helpers
 
 fun tierUpdateMessage() = """
-      {
-        "Type": "Notification", 
-        "MessageId": "48e8a79a-0f43-4338-bbd4-b0d745f1f8ec", 
-        "Token": null, 
-        "TopicArn": "arn:aws:sns:eu-west-2:000000000000:hmpps-domain-events", 
-        "Message": "{\"crn\":\"12345\",\"calculationId\":\"e45559d1-3460-4a0e-8281-c736de57c562\"}", 
-        "SubscribeURL": null, 
-        "Timestamp": "2021-03-05T11:23:56.031Z", 
-        "SignatureVersion": "1", 
-        "Signature": "EXAMPLEpH+..", 
-        "SigningCertURL": "https://sns.us-east-1.amazonaws.com/SimpleNotificationService-0000000000000000000000.pem"}      
+{
+  "Type": "Notification",
+  "MessageId": "f39059e7-a62d-4157-929a-fb049015c993",
+  "Token": null,
+  "TopicArn": "arn:aws:sns:eu-west-2:000000000000:hmpps-domain",
+  "Message": "{\"crn\":\"12345\",\"calculationId\":\"e45559d1-3460-4a0e-8281-c736de57c562\"}",
+  "SubscribeURL": null,
+  "Timestamp": "2021-10-21T06:27:57.028Z",
+  "SignatureVersion": "1",
+  "Signature": "EXAMPLEpH+..",
+  "SigningCertURL": "https://sns.us-east-1.amazonaws.com/SimpleNotificationService-0000000000000000000000.pem",
+  "MessageAttributes": {
+    "eventType": {
+      "Type": "String",
+      "Value": "TIER_CALCULATION_COMPLETE"
+    }
+  }
+}   
 """.trimIndent()
