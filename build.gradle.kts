453e863e05dfc0d072f0f986aed5ecebce080842
carlov20 <carlov20@users.noreply.github.com>
2021-12-13 12:34:25 +0000

    upgraded to latest dps gradle spring boot plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.13-beta-2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.14"
   kotlin("plugin.spring") version "1.5.0"
   jacoco
   id("io.gitlab.arturbosch.detekt").version("1.17.1")
