4621ad753e1df1978ec6bd7d1d6b91337030331b
carlov20 <carlov20@users.noreply.github.com>
2022-05-04 08:48:24 +0100

    FIXBUILD: security upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.4"
   kotlin("plugin.spring") version "1.6.20"
   jacoco
-  id("io.gitlab.arturbosch.detekt").version("1.19.0")
+  id("io.gitlab.arturbosch.detekt").version("1.20.0")
 }
 
 configurations {
